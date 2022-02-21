38d3d4fa3530db6c7196267687e33ece6001f85f
Alexander James Phillips <ajp.global@gmail.com>
2022-02-21 18:18:48 +0000

    2022-02-21
    Use cursor grab for canvas move

--- dist/about/AboutSidePanel.js
+++ dist/about/AboutSidePanel.js
@@ -7,6 +7,6 @@ const connector = connect(map_state);
 function _AboutSidePanel(props) {
   return /* @__PURE__ */ h("div", null, /* @__PURE__ */ h("span", {
     className: "description_label"
-  }, "Version"), " ", /* @__PURE__ */ h("b", null, "2022-02-18"));
+  }, "Version"), " ", /* @__PURE__ */ h("b", null, "2022-02-21"));
 }
 export const AboutSidePanel = connector(_AboutSidePanel);
